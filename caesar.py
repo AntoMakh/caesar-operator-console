4cdddf445b01b98391bd8715a9fa3ded41a2abbc
Chris-Acc <91938481+Chris-A-75@users.noreply.github.com>
2026-03-17 00:04:37 +0200

    Adds functionality for info: can get info for tool when no argument is passed. Fixes #2

--- caesar.py
+++ caesar.py
@@ -259,8 +259,11 @@ Welcome to the Caesar Operator Console. Type help to list commands.
 
     def do_info(self, arg):
         if arg.strip() == "":
-            print("Usage: info <tool>")
-            return False
+            if self.check_if_tool_selected():
+             print("Usage: info <tool>")
+             return False
+            else:
+                arg = self.current_tool
         tool = arg.split()[0]
         if tool not in self.tools:
             print("Tool not found: " + tool)
